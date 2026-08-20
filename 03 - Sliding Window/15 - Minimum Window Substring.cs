d8b323975e92ce14ab50a967d2d24b85e28bf338
klamomd <35202056+klamomd@users.noreply.github.com>
2026-08-20 03:31:04 -0600

    Went back to the whiteboard and got #15 MOSTLY working! But it fails on test case #44 - the really huge one :\

--- 03 - Sliding Window/15 - Minimum Window Substring.cs
+++ 03 - Sliding Window/15 - Minimum Window Substring.cs
@@ -6,9 +6,8 @@ public class Solution {
 			return s.Contains(t[0]) ? t : "";
 		}
 		
-		// I'm using both an int and string to track minimum substring because I want the default value to be an empty string without screwing up the math when checking if the first substring is better than the current minimum.
-		int bestMinimum = int.MaxValue;
-		string minString = "";
+		// Minimum window substring found.
+		string minString = null;
 		
 		// Number of characters left to find in s.
 		int charsLeft = t.Length;
@@ -20,226 +19,99 @@ public class Solution {
 		Dictionary<char, int> remaining = t
 			.GroupBy(c => c)
 			.ToDictionary(g => g.Key, g => g.Count());
+			
+		// Increment left pointer until we find the first character from T.
+		while (l < s.Length)
+		{
+			if (remaining.ContainsKey(s[l]))
+			{
+				break;
+			}
+			
+			l++;
+		}
 		
-		// Loop while the left pointer is still in bounds.
-		while (l < s.Length) {
-			while (l < s.Length) {
-				// Increment L until we find a char from t.
-				if (remaining.ContainsKey(s[l])) {
-					break;
-				}
-				
-				l++;
-			}
-				
-			/* {
-				// TODO - Should the block of code below be done in the R loop?
-				// TODO - Should the block of code below be done in the R loop?
-				// TODO - Should the block of code below be done in the R loop?
-				
-					// SANITY CHECK: Make sure remaining count of s[l] is > 0?
-					// 		Does it even matter? If t is "XXY" and S is "XXXXXXXXY", it doesn't matter that we've found too many Xs before we found a Y. (We'll find the shorter substring once we shrink L).
-					
-					// Add the current character to our window. Reduce the remaining count by 1, and decrement the number of chars left.
-					
-					remaining[s[l]]--;
-					// remaining[s[l]] -= 1; ?
-						
-					charsLeft--;
-					
-					break;
-			} */
-			
-			// If left pointer has gone past right, then reset right pointer.
-			if (l > r)
+		// EDGE CASE: If left pointer is now OOB, there are no characters from T in S.
+		if (l >= s.Length)
+		{
+			return "";
+		}
+		
+		// Update right pointer to match left pointer.
+		r = l;
+		
+		// Loop until R falls OOB.
+		while (l < s.Length && r < s.Length)
+		{
+			// GROW:
+			while (r < s.Length && charsLeft > 0)
 			{
-				// DEBUG:
-				Console.WriteLine($"SANITY CHECK: left pointer ({l}) has exceeded right pointer ({r})!");
-				
-				r = l;
-				// TODO - Will this break any math? Is there any situation where this will occur outside of the first iteration?
-			}
-			
-			// TODO - R OOB reset?
-			/*
-				If t is "XXY" and S is "XXXXXXXXY", R will fall OOB but we will need to keep shrinking until L is pointing to -3!
-			
-			*/
-			
-			
-			while (r < s.Length)
-			{
-				// Increment R until we find a char from t.
-				if (!remaining.ContainsKey(s[r])) {
-					r++;
-					continue;
+				// Matching character found at R.
+				if (remaining.ContainsKey(s[r]))
+				{
+					// Update remaining count.
+					remaining[s[r]]--;
+					
+					// If this was not a redundant char (count did not go below 0), then update the number of remaining chars too.
+					if (remaining[s[r]] >= 0) {
+						charsLeft--;
+					}
 				}
-				
-				
-				// TODO - SAME BUG AS BEFORE - DOUBLE-DIPPING HERE AFTER SHRINKING LEFT!
-				// TODO - SAME BUG AS BEFORE - DOUBLE-DIPPING HERE AFTER SHRINKING LEFT!
-				// TODO - SAME BUG AS BEFORE - DOUBLE-DIPPING HERE AFTER SHRINKING LEFT!:
-				
-				// Update the count for this character.
-				remaining[s[r]]--;
-				
-				// If the character's count fell below 0, then it can still be part of our current window as a redundant character, but we'll do no further processing apart from incrementing the right pointer.
-				if (remaining[s[r]] < 0) {
-					r++;
-					continue;
-				}
-				
-				// Otherwise, decrement the number of chars left to find.
-				charsLeft--;
-				
-				// If the number of chars to find has reached 0, then we've found all the chars in t.
-				if (charsLeft == 0) {
-					int currentLength = r - l + 1;
 
-					// Update the best minimum substring if appropriate.					
-					if (currentLength < bestMinimum) {
-						// TODO - DELETE - Using Java-style substring:
-						// minString = s.Substring(l, r + 1);
+				// If the number of remaining chars reaches 0, we have found a substring containing all chars in T.
+				if (charsLeft == 0) {
+					// Update best if appropriate.
+					int currentLength = r - l + 1;
+					if (minString == null || currentLength < minString.Length)
+					{
 						minString = s.Substring(l, currentLength);
-						bestMinimum = currentLength;
 					}
 					
-					// Break out of the right pointer loop.
+					// Break out of the "grow" loop.
 					break;
 				}
 				
-				// Increment the right pointer.
+				// Increment R.
 				r++;
 			}
 			
-			// Remove the left-most character from our window before resuming the left pointer loop.
-			remaining[s[l]]++;
-
-			// If this wasn't a redundant character (i.e. if the remaining number of this character is now > 0), also increment the number of chars left to find.
-			if (remaining[s[l]] > 0)
+			// If R fell OOB, then the current substring does not contain all chars in T, and we can skip the "shrink" loop.
+			if (r >= s.Length)
 			{
-				charsLeft++;
+				break;
 			}
-			
-			// Finally, increment the left pointer.
-			l++;
-			
-			// TODO - LAZY FIX FOR DOUBLE-DIPPING BUG - BETTER SOLUTION???
-			// TODO - LAZY FIX FOR DOUBLE-DIPPING BUG - BETTER SOLUTION???
-			// TODO - LAZY FIX FOR DOUBLE-DIPPING BUG - BETTER SOLUTION???
-			// Remove the right-most character from our window to make sure we don't include it twice (if it is not OOB).
-			if (r < s.Length)
-			{
-				remaining[s[r]]--;
-				charsLeft++;	
+				
+			// SHRINK:
+			while (l < s.Length && charsLeft == 0) {
+				// Remove the character at L from the substring.
+				// If the character exists in T, update the remaining count.
+				if (remaining.ContainsKey(s[l])) {
+					remaining[s[l]]++;
+					
+					// If this was not a redundant char (remaining count is now > 0), then increment the number of chars we have left to find.
+					if (remaining[s[l]] > 0) {
+						charsLeft++;
+					}
+				}
+				
+				// Increment L.
+				l++;
+				
+				// If the number of chars we have left to find is still 0, then this is still a substring containing all chars in T.
+				if (charsLeft == 0) {
+					// Update best if appropriate.
+					int currentLength = r - l + 1;
+					if (minString == null || currentLength < minString.Length)
+					{
+						minString = s.Substring(l, currentLength);
+					}
+				}
 			}
-			
-			// TODO - Can we just kick out if R is OOB?
-			// TODO - Can we just kick out if R is OOB?
-			// TODO - Can we just kick out if R is OOB?
+				
+			// Increment R, to avoid reprocessing the right-most character if/when we enter the "grow" loop again.
+			r++;
 		}
 		
-		// Return the minimum string found.
-		return minString;
-		
-		/*
-			Planning
-			
-			Convert T to a dictionary mapping char to remaining count
-			
-			Keep track of charsLeft ("tLength")
-			
-			
-			while (L < OOB) {
-				while (L < OOB) {
-				
-				
-					// LEFTOFF - SECOND LOOP ITERATION
-					// LEFTOFF - 
-					// LEFTOFF - Need to update left loop to "add back" any characters that fall out of the window as we increment left pointer!!
-					
-					
-					// ^^^^^^^^^^^^^^^^^
-					// ^^^^^^^^^^^^^^^^^
-					// ^^^^^^^^^^^^^^^^^
-					// ^^^^^^^^^^^^^^^^^
-					
-					
-					
-				
-					// Increment L until we find the first matching char (char exists as a key in $remaining)
-					if (!remaining.ContainsKey(s[l])) {
-						l++;
-						continue;
-					}
-					
-					
-					
-					// TODO - Should the block of code below be done in the R loop?
-					// TODO - Should the block of code below be done in the R loop?
-					// TODO - Should the block of code below be done in the R loop?
-					
-						// SANITY CHECK: Make sure remaining count of s[l] is > 0?
-						// 		Does it even matter? If t is "XXY" and S is "XXXXXXXXY", it doesn't matter that we've found too many Xs before we found a Y. (We'll find the shorter substring once we shrink L).
-						
-						// Add the current character to our window. Reduce the remaining count by 1, and decrement the number of chars left.
-						
-						remaining[s[l]]--;
-						// remaining[s[l]] -= 1; ?
-							
-						charsLeft--;
-						
-						break;
-				}
-				
-				// SANITY CHECK: If r is before L, then set r to L!
-				// SANITY CHECK: If r is before L, then set r to L!
-				// SANITY CHECK: If r is before L, then set r to L!
-				
-				
-				// LEFTOFF - Got confused about left / right pointer order and process...
-				
-				
-				while (R < OOB) {
-					// Increment R until we find a matching char (char exists as a key in $remaining).
-					if (!remaining.ContainsKey(s[r])) {
-						r++;
-						continue;
-					}
-					
-					// Update the count for this character.
-					remaining[s[r]]--;
-					
-					// If the character's count fell below 0, then it can still be part of our current window, but we'll do no further processing.
-					if (remaining[s[r]] < 0) {
-						continue;
-					}
-					
-					// Otherwise, decrement the number of chars left.
-					charsLeft--;
-					
-					// If the number of chars has reached 0, then we've found all the chars in $t.
-					if (charsLeft == 0) {
-						// Update the minimum substring if appropriate.
-						int currentLength = r - l + 1;
-						
-						if (currentLength < bestMinimum) {
-							minString = s.Substring(l, r + 1);
-						}
-						
-						// Break out of the right pointer loop.
-						break;
-					}
-				}
-				
-				
-				// Remove the left-most character from the window.
-				remaining[s[l]]++;
-				charsLeft++;
-				l++;
-			}
-			
-			return best;
-		*/
+		return minString == null ? "" : minString;
 	}
 }
